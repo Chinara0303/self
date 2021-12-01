b87d6edd9d1438ca4ceea22100370aa9aca8ca52
[Chinara0303] <chinaraei@code.edu.az>
2021-12-02 01:49:19 +0400

    me

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ikiededin cemi
+            //int a = 15;
+            //int b = 16;
+            //Console.WriteLine(a+b);
+
+            //Console.WriteLine(Sum(16,15));
+
+
+            // bosluqlari sayma
+            //Console.WriteLine(Countspace("Chinara  Ibadova"));
+
+            //arrayin elementlerinin cemi
+            //int[] arr = { 3, 5, 9, 2, 10 };
+            //int sum = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    sum += arr[i];
+            //}
+            //Console.WriteLine(sum);
+
+            //daxil edilen ededlerin yerlerinin deyisilmesi
+            //int num1 = 54;
+            //int num2 = 45;
+            //Interchange(ref num1, ref num2);
+            //Console.WriteLine(num2);
+            //Console.WriteLine($"num1-{num1}");
+
+            //quvvet
+            //Console.WriteLine(Multiply(5,4));
+
+            //daxil edilen ededin reqemlerinin cemi
+            //int num = 4567;
+            //Console.WriteLine(Sum1(num));
+
+            //factorial
+            //decimal num = 3;
+            //Factorial(num);
+            //Console.WriteLine(Factorial(num));
+
+
+
+
+        }
+        //ikiededin cemi
+        //static int Sum(int a, int b)
+        //{
+        //    int result = 0;
+        //    result = a + b;
+        //    return result;
+
+        //}
+
+
+
+        // bosluqlari sayma
+        //static int Countspace(string word)
+        //{
+
+        //    int count = 0;
+        //    for (int i = 0; i < word.Length; i++)
+        //    {
+        //       if(word[i]==' ')
+        //        {
+        //            count++;
+        //        }
+
+        //    }
+        //    return count;
+        //}
+
+
+        //static int Countspace(string word)
+        //{
+        //    int count = 0;
+        //    foreach (char item in word)
+        //    {
+        //        if (item == ' ')
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    return count;
+        //}
+
+
+
+
+        //daxil edilen ededlerin yerlerinin deyisilmesi
+        //static void Interchange(ref int num1, ref int num2)
+        // {
+        //    int newnum;
+
+        //    newnum = num1;
+        //    num1 = num2;
+        //    num2 = newnum;
+        //    Console.WriteLine(num1);
+        //    //Console.WriteLine($"num2-{num2}");
+
+        // }
+
+        //quvvet
+        //static int Multiply(int a, int k)
+        //{
+        //    int result = 1;
+        //    for (int i = 0; i < k; i++)
+        //    {
+        //        result = result * a;
+        //    }
+        //    return result;
+        //}
+
+        //daxil edilen ededin reqemlerinin cemi
+        //static int Sum1(int num)
+        //{
+        //    string n1 = Convert.ToString(num);
+        //    int result = 0;
+        //    for (int i = 0; i <n1.Length; i++)
+        //    {
+        //        result = result + Convert.ToInt32(n1.Substring(i, 1));
+        //    }
+        //    return result;
+        //}
+
+
+        //factorial
+        //static decimal Factorial(decimal num)
+        //{
+        //    int result = 1;
+        //    if (num == 0)
+        //    {
+        //        return result;
+        //    }
+        //    else
+        //    {
+        //       return num * Factorial(num - 1);
+        //    }
+
+        //}
+
+    }
+
+
+}
